2560e3f6a1322e4398bef4716169e4d5b7eb2337
Sunil Kumar K (MS/ETB72-PS) <uql1kor@bosch.com>
2022-09-13 09:52:52 +0530

    jkfj

--- CustomField-Plugin/src/index.jsx
+++ CustomField-Plugin/src/index.jsx
@@ -21,7 +21,7 @@ let newbody2 =
  
     "fields": {
 
-      fields: Object { customfield_10033:  ${parseInt(formData.newstory)}}
+      fields: Object { customfield_10033:  formData.newstory}
     }
 }`;
 console.log("Body created by the issueid is= " + JSON.stringify(newbody2));
