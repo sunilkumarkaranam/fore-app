ce942094bfc0e4d288121a79b9602defd02f2700
sunilkumarkaranam <uql1kor@bosch.com>
2022-10-20 15:29:05 +0530

    ydsgf

--- treeforjira/static/hello-world/src/App.js
+++ treeforjira/static/hello-world/src/App.js
@@ -1,17 +1,20 @@
 import React, { useEffect, useState } from 'react';
 import { invoke } from '@forge/bridge';
+import React from 'react';
 
+import TableTree, { Header, Headers, Rows } from '@atlaskit/table-tree';
 function App() {
-    const [data, setData] = useState(null);
-
-    useEffect(() => {
-        invoke('getText', { example: 'my-invoke-variable' }).then(setData);
-    }, []);
+   
 
     return (
-        <div>
-            {data ? data : 'Loading...'}
-        </div>
+        <TableTree>
+    <Headers>
+      <Header width={200}>Chapter title</Header>
+      <Header width={100}>Numbering</Header>
+      <Header width={100}>Page</Header>
+    </Headers>
+    <Rows items={undefined} render={() => null} />
+  </TableTree>
     );
 }
 
