3b2e0d8fa52e3aa53bc2813143f62a4d0735d29c
sunilkumarkaranam <uql1kor@bosch.com>
2023-01-02 18:04:19 +0530

    jhgew

--- syncfusiontraill/static/hello-world/src/App.js
+++ syncfusiontraill/static/hello-world/src/App.js
@@ -1,16 +1,35 @@
 import React, { useEffect, useState } from 'react';
 import { invoke } from '@forge/bridge';
-
+import { TreeGridComponent, ColumnsDirective, ColumnDirective, DataStateChangeEventArgs, Selection, RowDD, Inject } from "@syncfusion/ej2-react-treegrid";
+import { ButtonComponent } from '@syncfusion/ej2-react-buttons';
+import { issueData, findChildByJql } from "./data/fetchData";
+import './App.css';
 function App() {
-    const [data, setData] = useState(null);
+    const [dataSource, setDataSource] = useState(null);
+  let projects = "TEST";
+  let issueLinkType = {
+    id: "10008",
+    name: "Track/Contributes",
+    inward: "Contributes To",
+    outward: "Tracks"
+  }
+  let treegridIssue;
 
-    useEffect(() => {
-        invoke('getText', { example: 'my-invoke-variable' }).then(setData);
-    }, []);
+  const handleClickSearch = async () => {
+    if(treegridIssue) {
+      treegridIssue.showSpinner(); // show the spinner
+      let value = await issueData(projects, issueLinkType, "");
+      treegridIssue.hideSpinner(); // hide the spinner  
+      setDataSource(value);
+    }
+  };
 
     return (
         <div>
-            {data ? data : 'Loading...'}
+             <TreeGridComponent>
+             ref={g => treegridIssue = g}
+            dataSource={dataSource}
+             </TreeGridComponent>
         </div>
     );
 }
