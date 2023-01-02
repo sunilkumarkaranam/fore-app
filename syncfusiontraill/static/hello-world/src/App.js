2b3c95ab3f4305277e67bf91380420f3c81db4c5
sunilkumarkaranam <uql1kor@bosch.com>
2023-01-02 23:39:59 +0530

    iaue

--- syncfusiontraill/static/hello-world/src/App.js
+++ syncfusiontraill/static/hello-world/src/App.js
@@ -5,31 +5,33 @@ import { ButtonComponent } from '@syncfusion/ej2-react-buttons';
 import { issueData, findChildByJql } from "./data/fetchData";
 import './App.css';
 function App() {
-    const [dataSource, setDataSource] = useState(null);
-  let projects = "TEST";
-  let issueLinkType = {
-    id: "10008",
-    name: "Track/Contributes",
-    inward: "Contributes To",
-    outward: "Tracks"
-  }
-  let treegridIssue;
-
-  const handleClickSearch = async () => {
-    if(treegridIssue) {
-      treegridIssue.showSpinner(); // show the spinner
-      let value = await issueData(projects, issueLinkType, "");
-      treegridIssue.hideSpinner(); // hide the spinner  
-      setDataSource(value);
-    }
-  };
-
+  
     return (
         <div>
-             <TreeGridComponent>
-             ref={g => treegridIssue = g}
-            dataSource={dataSource}
-             </TreeGridComponent>
+             <TreeGridComponent>  
+                <ColumnsDirective>
+              <ColumnDirective
+                field="key"
+                headerText="Issue Key"
+              ></ColumnDirective>
+              <ColumnDirective
+                field="summary"
+                headerText="Summary"
+              ></ColumnDirective>
+              <ColumnDirective
+                field="issueType"
+                headerText="Issue Type"
+              ></ColumnDirective>
+              <ColumnDirective
+                field="assignee"
+                headerText="Assignee"
+              ></ColumnDirective>
+              <ColumnDirective
+                field="storyPoint"
+                headerText="Story Point"
+              ></ColumnDirective>
+            </ColumnsDirective>
+            <Inject services={[RowDD, Selection]} /></TreeGridComponent>
         </div>
     );
 }
