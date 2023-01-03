ff69a00418a9d5307722e4bb8c083692afe53097
sunilkumarkaranam <uql1kor@bosch.com>
2023-01-04 00:27:38 +0530

    jsahf

--- Synfusion/static/hello-world/src/App.js
+++ Synfusion/static/hello-world/src/App.js
@@ -1,16 +1,79 @@
 import React, { useEffect, useState } from 'react';
 import { TreeGridComponent, ColumnsDirective, ColumnDirective, DataStateChangeEventArgs, Selection, RowDD, Inject } from "@syncfusion/ej2-react-treegrid";
 import { ButtonComponent } from '@syncfusion/ej2-react-buttons';
-import { summaryDat } from "./data/fetchData";
-//import { summaryDat } from './data';
+import { issueData, findChildByJql } from "./data/fetchData";
 import './App.css';
 
 function App() {
+  const [dataSource, setDataSource] = useState(null);
+  let projects = "TEST";
+  let issueLinkType = {
+    id: "10008",
+    name: "Track/Contributes",
+    inward: "Contributes To",
+    outward: "Tracks"
+  }
+  let treegridIssue;
+
+  const handleClickSearch = async () => {
+    if(treegridIssue) {
+      treegridIssue.showSpinner(); // show the spinner
+      let value = await issueData(projects, issueLinkType, "");
+      treegridIssue.hideSpinner(); // hide the spinner  
+      setDataSource(value);
+    }
+  };
+
+  const handleExpand = async (dataState) => {
+    treegridIssue && treegridIssue.showSpinner(); // show the spinner
+    const data = await findChildByJql(projects, issueLinkType, dataState.data);
+    treegridIssue && treegridIssue.hideSpinner(); // hide the spinner  
+    return data;
+  }
+
+  const handleDataStateChange = (dataState) => {
+    console.log(dataState);
+    if (dataState.requestType === 'expand') {
+      handleExpand(dataState).then((childData) => {
+        dataState.childData = childData;
+        dataState.childDataBind();
+      });
+    } else {
+      console.log("else");
+      issueData(projects, issueLinkType, "").then((data) => {
+        treegridIssue && setDataSource(data);
+      });
+    }
+  }
+
+  const handleRowDrop = (rowDragEventArgs) => {
+    // console.log(rowDragEventArgs);
+    // let currentViewRecords = treegridIssue.getCurrentViewRecords();
+    // let target = currentViewRecords[rowDragEventArgs.dropIndex];
+    // console.log(currentViewRecords);
+    // console.log(target);
+      console.log("handleRowDrop");
+  }
+
   return (
     <div>
-    
+      <div>
+        <ButtonComponent cssClass='e-info' onClick={handleClickSearch}>Search</ButtonComponent>
+      </div>
+      <div className="control-pane">
+        <div className="control-section">
           <TreeGridComponent
-         dataSource={summaryDat}
+            ref={g => treegridIssue = g}
+            dataSource={dataSource}
+            dataStateChange={handleDataStateChange}
+            treeColumnIndex={0}
+            enableCollapseAll="true"
+            idMapping='key'
+            parentIdMapping="ParentItem"
+            hasChildMapping="isParent"
+            allowRowDragAndDrop={true}
+            childMapping="childIssues"
+            rowDrop={handleRowDrop}
           >
             <ColumnsDirective>
               <ColumnDirective
@@ -37,6 +100,8 @@ function App() {
             <Inject services={[RowDD, Selection]} />
           </TreeGridComponent>
         </div>
+      </div>
+    </div>
   );
 }
 
